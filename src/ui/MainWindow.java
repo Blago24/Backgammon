3665fa9b747a393770850f6a51dfac0508ff4e66
Blago24 <blago102@gmail.com>
2017-05-11 19:50:11 +0300

    background added

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -1,15 +1,24 @@
 package ui;
 
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import models.Bord;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
 public class MainWindow {
 	 private Bord bord;
 	private JFrame frame;
-
+	
 	/**
 	 * Launch the application.
 	 */
@@ -34,8 +43,14 @@ public class MainWindow {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.PINK);
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\background.jpg"));
+		lblNewLabel.setBounds(0, 0, 900, 690);
+		frame.getContentPane().add(lblNewLabel);
+		frame.setBounds(200, 200, 900, 690);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
 	}
-
 }
