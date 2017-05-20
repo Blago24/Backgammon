749ea103db23d33e51129fe96ccd5601e45b45f9
Blago24 <blago102@gmail.com>
2017-05-20 15:31:41 +0300

    visualisation

--- src/models/Bord.java
+++ src/models/Bord.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 
 public class Bord {
 	final static int bordRows = 2;
-	final static int bordCols = 10;
+	final static int bordCols = 12;
 	final static int BlackPiece = 0;
 	final static int WhitePiece = 1;
 	final static int TopRow = 0;
 	final static int BotRow = 1;
+	// one piece , two pieces , three pieces , four and five
+	final static int[] topRowYPositions = { -275, -206, -137, -69, 1 };
+	final static int botRowYPosition = 345;
+	final static int[] rowXPosition = { 6, 77, 148, 219, 290, 361, 468, 539, 610, 681, 752, 823 };
+
+	public static int getRowXposition(int col) {
+		return rowXPosition[col];
+
+	}
 
 	// ArrayList <Piece> [][] pieces = new ArrayList[bordRows][bordCols];
 	ArrayList<Piece>[][] pieces = new ArrayList[bordRows][bordCols];;
@@ -26,42 +35,45 @@ public class Bord {
 		}
 
 	}
- public void show(){
-	 for (int i = 0; i < pieces.length; i++) {
-		for (int j = 0; j < pieces[0].length; j++) {
-			System.out.print(" "+pieces[i][j].size());
-					
-		}
-		System.out.println();
-	}
- }
- public void showColor(){
-	 for (int i = 0; i < pieces.length; i++) {
-		for (int j = 0; j < pieces[0].length; j++) {
-			
-			if(pieces[i][j].size()!=0){
-				System.out.print(this.pieces[i][j].get(0).getColor());
-			}else{
-				System.out.print("*");
+
+	public void show() {
+		for (int i = 0; i < pieces.length; i++) {
+			for (int j = 0; j < pieces[0].length; j++) {
+				System.out.print(" " + pieces[i][j].size());
+
 			}
-			
+			System.out.println();
 		}
-		System.out.println();
 	}
- }
+
+	public void showColor() {
+		for (int i = 0; i < pieces.length; i++) {
+			for (int j = 0; j < pieces[0].length; j++) {
+
+				if (pieces[i][j].size() != 0) {
+					System.out.print(this.pieces[i][j].get(0).getColor());
+				} else {
+					System.out.print("*");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+
 	public ArrayList<Piece>[][] getPieces() {
-	return pieces;
-}
+		return pieces;
+	}
 
 	public void iniBord() {
 		for (int row = 0; row < pieces.length; row++) {
 			for (int col = 0; col < pieces[0].length; col++) {
 				int numberOfPieces = numberOfPiecesInCurrentCol(col);
 				if (row == 0) {
-					
+
 					fillTopRow(col, numberOfPieces);
 				} else {
-					
+
 					fillBotRow(col, numberOfPieces);
 				}
 
@@ -71,28 +83,28 @@ public class Bord {
 
 	private int numberOfPiecesInCurrentCol(int col) {
 		switch (col) {
-		case 0:
-		case 5:
-			return 5;
-		case 3:
-			return 3;
-		case 9:
+		case 11:
 			return 2;
+		case 4:
+			return 3;
+		case 0:
+		case 6:
+			return 11;
 
 		}
-		return 0;
+		return -1;
 	}
 
 	private void fillTopRow(int col, int numberOfPieces) {
 		switch (col) {
 		case 0:
-		case 9:
-			//System.out.println("white");
+		case 11:
+			// System.out.println("white");
 			fillColWithWhitePieces(TopRow, col, numberOfPieces);
 			break;
-		case 3:
-		case 5:
-			//System.out.println("black");
+		case 4:
+		case 6:
+			// System.out.println("black");
 			fillColWithBlackPieces(TopRow, col, numberOfPieces);
 			break;
 		}
@@ -114,17 +126,49 @@ public class Bord {
 	private void fillBotRow(int col, int numberOfPieces) {
 		switch (col) {
 		case 0:
-		case 9:
-			//System.out.println("black");
+		case 11:
+			// System.out.println("black");
 			fillColWithBlackPieces(BotRow, col, numberOfPieces);
 			break;
-		case 3:
-		case 5:
-			//System.out.println("white");
+		case 4:
+		case 6:
+			// System.out.println("white");
 			fillColWithWhitePieces(BotRow, col, numberOfPieces);
 			break;
 		}
 
+	}
+
+	public int checkTheNumberOfPiecesOnCurrentPosition(int row, int col) {
+
+		return pieces[row][col].size();
+	}
+
+	public boolean checkTheColorOfPiecesOnCurrentPosition(int row, int col) {
+		if (pieces[row][col].get(0).getColor() == WhitePiece) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getYPosition(int row, int sizeOfCurrentPosition) {
+
+		if (row == BotRow) {
+			return botRowYPosition;
+		} else {
+			if(sizeOfCurrentPosition>5){
+				return topRowYPositions[topRowYPositions.length-1];
+			}else{
+				return topRowYPositions[sizeOfCurrentPosition-1];
+			}
+			
+		}
+
+	}
+
+	public int getXPosition(int col, int counterForPics) {
+		// counterForPics*4 is for the shift
+		return getRowXposition(col) + counterForPics * 4;
 	}
 
 }
--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -1,28 +1,36 @@
 package ui;
 
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
 import models.Bord;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
+import java.awt.Rectangle;
 
 public class MainWindow {
-	 private Bord bord;
+	private Bord bord;
 	private JFrame frame;
-	
+	// private JLabel oneWhite = null;
+	// private JLabel twoWhite = null;
+	// private JLabel threeWhite = null;
+	// private JLabel fourWhite = null;
+	// private JLabel fiveWhite = null;
+	// private JLabel oneBlack = null;
+	// private JLabel twoBlack = null;
+	// private JLabel threeBlack = null;
+	// private JLabel fourBlack = null;
+	// private JLabel fiveBlack = null;
+	private JLabel currentPiece = null;
+
+	private JLabel background = null;
+
 	/**
 	 * Launch the application.
 	 */
-
 
 	public JFrame getFrame() {
 		return frame;
@@ -35,6 +43,141 @@ public class MainWindow {
 		this.bord = bord;
 		bord.show();
 		initialize();
+
+		showBord();
+	}
+
+	public void showBord() {
+		int sizeOfCurrentPosition;
+		boolean isItWhite;
+		for (int row = 0; row < 2; row++) {
+			for (int col = 0; col < 12; col++) {
+				
+				sizeOfCurrentPosition = bord.checkTheNumberOfPiecesOnCurrentPosition(row, col);
+				System.out.println(sizeOfCurrentPosition);
+				if(sizeOfCurrentPosition>0){
+					isItWhite = bord.checkTheColorOfPiecesOnCurrentPosition(row, col);
+					showPictureForCurrentPosition(row, col, sizeOfCurrentPosition, isItWhite);
+				}
+				
+
+			}
+		}
+
+		frame.getContentPane().add(background);
+	}
+
+	private void showPictureForCurrentPosition(int row, int col, int sizeOfCurrentPosition, boolean isItWhite) {
+		int numberOfPictures = numberOfPicturesAtCurrentPosition(sizeOfCurrentPosition);
+
+		for (int counterForPics = 0; counterForPics < numberOfPictures; counterForPics++) {
+			
+			if (isItWhite == true) {
+				chooseWhitePiecesPicture(row, col, sizeOfCurrentPosition, counterForPics);
+			} else {
+				chooseBlackPiecesPicture(row, col, sizeOfCurrentPosition, counterForPics);
+			}
+			sizeOfCurrentPosition-=5;
+			frame.getContentPane().add(currentPiece);
+			
+		}
+		
+		// frame.getContentPane().add(oneWhite);
+		// frame.getContentPane().add(twoWhite);
+		// frame.getContentPane().add(threeWhite);
+		// frame.getContentPane().add(fourWhite);
+		// frame.getContentPane().add(fiveWhite);
+		// frame.getContentPane().add(oneBlack);
+	}
+
+	private void chooseBlackPiecesPicture(int row, int col, int sizeOfCurrentPosition, int counterForPics) {
+
+		currentPiece = new JLabel("");
+		currentPiece.setText("");
+
+		currentPiece.setBounds(new Rectangle(bord.getXPosition(col, counterForPics),
+				bord.getYPosition(row, sizeOfCurrentPosition), 69, 345));
+		
+		String pathToPicture;
+		if(sizeOfCurrentPosition>5){
+			 pathToPicture = getPathToBlackPictures(5);
+		}else{
+			pathToPicture=  getPathToBlackPictures(sizeOfCurrentPosition);
+		}
+		currentPiece.setIcon(new ImageIcon(pathToPicture));
+
+	}
+
+	private String getPathToBlackPictures(int sizeOfCurrentPosition) {
+		switch (sizeOfCurrentPosition) {
+		case 1:
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\black1.png";
+
+		case 2:
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\black2.png";
+		case 3:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\black3.png";
+		case 4:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\black4.png";
+		case 5:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\black5.png";
+
+		}
+		return null;
+
+	}
+
+	private void chooseWhitePiecesPicture(int row, int col, int sizeOfCurrentPosition, int counterForPics) {
+
+		currentPiece = new JLabel("");
+		currentPiece.setText("");
+
+		currentPiece.setBounds(new Rectangle(bord.getXPosition(col, counterForPics),
+				bord.getYPosition(row, sizeOfCurrentPosition), 69, 345));
+		String pathToPicture;
+		if(sizeOfCurrentPosition>5){
+			 pathToPicture = getPathToWhitePictures(5);
+		}else{
+			pathToPicture= getPathToWhitePictures(sizeOfCurrentPosition);
+		}
+		
+		currentPiece.setIcon(new ImageIcon(pathToPicture));
+
+	}
+
+	private String getPathToWhitePictures(int sizeOfCurrentPosition) {
+		switch (sizeOfCurrentPosition) {
+		case 1:
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\white1.png";
+
+		case 2:
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\white2.png";
+		case 3:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\white3.png";
+		case 4:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\white4.png";
+		case 5:
+
+			return "C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\white5.png";
+
+		}
+		return null;
+
+	}
+
+	private int numberOfPicturesAtCurrentPosition(int sizeOfCurrentPosition) {
+		if (sizeOfCurrentPosition <= 5) {
+			return 1;
+		} else if (sizeOfCurrentPosition > 5 && sizeOfCurrentPosition <= 10) {
+			return 2;
+		} else {
+			return 3;
+		}
 	}
 
 	/**
@@ -44,13 +187,16 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.PINK);
 		frame.getContentPane().setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\background.jpg"));
-		lblNewLabel.setBounds(0, 0, 900, 690);
-		frame.getContentPane().add(lblNewLabel);
-		frame.setBounds(200, 200, 900, 690);
+
+		background = new JLabel("");
+		background.setIcon(new ImageIcon("C:\\Git\\BackgammonWorkspace\\Backgammon\\src\\images\\bg.jpg"));
+		background.setText("");
+		background.setBounds(0, 0, 910, 690);
+
+		frame.setBounds(200, 200, 909, 730);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 	}
+
+
 }
